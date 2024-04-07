4498229ea32b357b2c8609785503328bef792fb6
Eduard Galikhanov <godlik04@mail.ru>
2024-04-07 15:19:47 +0300

    feat: perceptron

---
+++ PerceptronXOR.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def activateFunction(x):
+    return 0 if x <= 0 else 1
+
+def network(C):
+    x = np.array([C[0], C[1], 1])
+    w1 = [1, 1, -1.5]
+    w2 = [1, 1, -0.5]
+    w_hidden = np.array([w1, w2])
+    w_out = np.array([-1, 1, 0.5])
+
+    sum = np.dot(x, w_hidden)
+    out = [activateFunction(x) for x in sum]
+    out.append(1)
+    out = np.array(out)
+
+    sum = np.dot(w_out, out)
+    y = activateFunction(sum)
+    return y
+
+C1 = [(1,0), (0,1)]
+C2 = [(0,0), (1,1)]
+
+print(network(C1[0]), network(C1[1]))
+print(network(C2[0]), network(C2[1]))
+
+
+
+
+
